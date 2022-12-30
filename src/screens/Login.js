a5443d510d40079c67afc98ac38dfee309d2007a
indrajith76 <indrajithgoswami@gmail.com>
2022-12-31 02:00:26 +0600

    added login page

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -5,52 +5,80 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
+  ScrollView,
 } from "react-native";
 
 export default function Login() {
   return (
-    <View style={styles.MainContainer}>
-      <Image
-        style={styles.loginImg}
-        source={require("../../assets/login-img.png")}
-      />
-      <View style={styles.formContainer}>
-        <Text style={styles.heading}>Login</Text>
+    <ScrollView>
+      <View style={styles.MainContainer}>
+        <Image
+          style={styles.loginImg}
+          source={require("../../assets/login-img.png")}
+        />
+        <View style={styles.formContainer}>
+          <Text style={styles.heading}>Login</Text>
 
-        <View style={[styles.inputContainer, { marginTop: 30 }]}>
+          <View style={[styles.inputContainer, { marginTop: 20 }]}>
+            <Image
+              style={styles.labelIconImg}
+              source={require("../../assets/icons/email.png")}
+            />
+            <TextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              style={styles.inputStyles}
+              placeholder="E-mail ID"
+            />
+          </View>
+
+          <View style={[styles.inputContainer, { marginTop: 20 }]}>
+            <Image
+              style={styles.labelIconImg}
+              source={require("../../assets/icons/lock.png")}
+            />
+            <TextInput
+              autoCapitalize="none"
+              autoCorrect={false}
+              style={styles.inputStyles}
+              secureTextEntry={true}
+              placeholder="Password"
+            />
+          </View>
+
+          <Text style={styles.forgotPass}>Forgot Password?</Text>
+
+          <TouchableOpacity style={styles.loginBtn}>
+            <Text style={styles.loginBtnText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Horizontal Rule */}
+        <View style={styles.hr}>
           <Image
-            style={styles.labelIconImg}
-            source={require("../../assets/icons/email.png")}
+            style={styles.hrLine}
+            source={require("../../assets/icons/line.png")}
           />
-          <TextInput
-            autoCapitalize="none"
-            autoCorrect={false}
-            style={styles.inputStyles}
-            placeholder="E-mail ID"
+          <View style={{ paddingHorizontal: 6 }}>
+            <Text style={{ fontSize: 13, color: "#1616163b" }}>OR</Text>
+          </View>
+          <Image
+            style={styles.hrLine}
+            source={require("../../assets/icons/line.png")}
           />
         </View>
 
-        <View style={[styles.inputContainer, { marginTop: 50 }]}>
-          <Image
-            style={styles.labelIconImg}
-            source={require("../../assets/icons/lock.png")}
-          />
-          <TextInput
-            autoCapitalize="none"
-            autoCorrect={false}
-            style={styles.inputStyles}
-            secureTextEntry={true}
-            placeholder="Password"
-          />
-        </View>
-
-        <Text style={styles.forgotPass}>Forgot Password?</Text>
-
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginBtnText}>Login</Text>
+        <TouchableOpacity style={styles.googleLoginBtn}>
+          <Image source={require("../../assets/icons/google.png")} />
+          <Text style={styles.googleLoginBtnText}>Login with Google</Text>
         </TouchableOpacity>
+
+        <View style={styles.signUpNavigator}>
+          <Text style={styles.navigatorText1}>Don’t have account? </Text>
+          <Text style={styles.navigatorText2}>Sign Up</Text>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
 
@@ -59,6 +87,7 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingLeft: 20,
     paddingRight: 20,
+    paddingBottom: 50,
   },
   loginImg: {
     width: 230,
@@ -104,5 +133,44 @@ const styles = StyleSheet.create({
   loginBtnText: {
     color: "#FFFFFF",
     textAlign: "center",
+  },
+  hr: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 40,
+  },
+  hrLine: {
+    width: "45%",
+  },
+  googleLoginBtn: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "#00000021",
+    paddingVertical: 10,
+    borderRadius: 6,
+  },
+  googleLoginBtnText: {
+    fontSize: 15,
+    marginLeft: 10,
+    color: "#282828",
+  },
+  signUpNavigator: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    marginLeft: "auto",
+    marginRight: "auto",
+    marginTop: 30,
+  },
+  navigatorText1: {
+    color: "#1616163b",
+    fontSize: 13,
+  },
+  navigatorText2: {
+    color: "#282828",
   },
 });

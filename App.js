c5499cac51300755279963d4f609041c35b47984
indrajith76 <indrajithgoswami@gmail.com>
2023-01-01 13:52:26 +0600

    final submission

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-        initialRouteName="Home"
+        initialRouteName="Login"
         screenOptions={{ 
           headerShown: false
          }}
